83154a1a0860be140285a34e03ca26402e86e4a0
manichandanreddy14 <141955690+manichandanreddy14@users.noreply.github.com>
2023-12-30 23:36:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let img=document.getElementsByClassName("imgs");
+let para=document.getElementsByClassName("para");
+let head=document.getElementsByClassName("head");
+function work(){
+
+    head.innerHtml="mani";
+}
